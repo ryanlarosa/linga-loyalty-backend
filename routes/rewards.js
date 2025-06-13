5ee179cc2c6e485a3e6e7018c6ece396d345dd26
PC <ryan.larosa08@gmail.com>
2025-06-13 16:29:50 +0400

    Added Rewards Route

--- routes/rewards.js
+++ routes/rewards.js
@@ -1,7 +1,7 @@
 // routes/rewards.js
 const express = require("express");
 const router = express.Router();
-const pool = require("../db"); // Assumes you have created the db.js file
+const pool = require("../db");
 
 // GET /api/rewards - Get list of all active rewards
 router.get("/", async (req, res) => {
